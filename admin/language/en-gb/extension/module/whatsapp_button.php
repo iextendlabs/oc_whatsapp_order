32da35e3c181e640663cef8dd8741ed7a2c75566
Ali Hamza <miangdpp@gmail.com>
2022-10-18 17:52:05 +0500

    initial commit

---
+++ admin/language/en-gb/extension/module/whatsapp_button.php
@@ -0,0 +1,19 @@
+<?php
+// Heading
+$_['heading_title']    = 'Whatsapp Button';
+
+// Text
+$_['text_extension']   = 'Extensions';
+$_['text_success']     = 'Success: You have modified Whatsapp Button module!';
+$_['text_edit']        = 'Edit Whatsapp Button Module';
+
+// Entry
+$_['entry_status']     = 'Status';
+$_['entry_whatsapp_number']= 'Whatsapp Number';
+$_['entry_whatsapp_message']= 'Whatsapp Message';
+$_['entry_whatsapp_code']= '+92XXXXXXXXXX';
+$_['entry_whatsapp_button_color']= 'Whatsapp Button Color';
+$_['entry_whatsapp_button_text']= 'Whatsapp Button Text';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify Whatsapp Button module!';
